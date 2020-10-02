a961d64bde56c2c8118a356e2d9fc7e9eec44a97
AleksLat <66917008+AleksLat@users.noreply.github.com>
2020-10-02 14:45:21 +0200

    Correct Link to GitHub

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/AleksLat/Green-Weather-App">
+      <a href="https://github.com/AleksLat/react-weather-app">
         Open-source code
       </a>
       by Aleks Lat
